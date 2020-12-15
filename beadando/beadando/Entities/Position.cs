feca25741e86550aa37bc44fcc1260692be154c7
Norbert Bencsik <norbert.bencsik@stud.uni-corvinus.hu>
2020-12-15 20:22:08 +0100

    elkezdés

---
+++ beadando/beadando/Entities/Position.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beadando.Entities
+{
+    public enum Position
+    {
+        Irányító= 1,
+        Bedobó = 2,
+        Kiscsatár = 3,
+        Erőcsatár = 4,
+        Center = 5
+    }
+
+    public class Players
+    { 
+            
+    }
+}
