6abe36c49d3513e559779e65d6f7ca47f271cdd2
Norbert Bencsik <norbert.bencsik@stud.uni-corvinus.hu>
2020-12-16 01:37:10 +0100

    valami excel

--- beadando/beadando/Form1.Designer.cs
+++ beadando/beadando/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
@@ -64,11 +65,21 @@
             this.numericUpDown1.Size = new System.Drawing.Size(35, 20);
             this.numericUpDown1.TabIndex = 3;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(166, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Szelektálás";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
@@ -86,6 +97,7 @@
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- beadando/beadando/Form1.cs
+++ beadando/beadando/Form1.cs
@@ -9,6 +9,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Reflection;
+using Excel = Microsoft.Office.Interop.Excel;
 
 
 namespace beadando
@@ -16,11 +18,75 @@ namespace beadando
     public partial class Form1 : Form
     {
         List<Players> Jatekosok = new List<Players>();
+
+
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
         public Form1()
         {
             InitializeComponent();
             Jatekosok = GetPlayers(AppDomain.CurrentDomain.BaseDirectory + @"\jatekosadatok\nbajatekosok.csv");
             dataGridView1.DataSource = Jatekosok.ToList();
+            CreateExcel();
+
+            CreateTable();
+
+        }
+
+        private static void CreateTable()
+        {
+            string[] headers = new string[]
+            {
+                "Név",
+                "Pozíció",
+                "Perc",
+                "Pontátlag",
+                "Percenként dobott pontátlag"
+            };
+            for (int i = 0; i < 290; i++)
+            {
+                xlSheet.Cells[1, 1] = headers[0];
+                object[,] values = new object[, headers.Length];
+            }
+            int counter = 0;
+            foreach (Players f in Jatekosok)
+            {
+                values[counter, 0] = f.Code;
+                // ...
+                values[counter, 8] = "";
+                counter++;
+            }
+        }
+
+        private void CreateExcel()
+        {
+            try
+            {
+
+                xlApp = new Excel.Application();
+
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+                xlSheet = xlWB.ActiveSheet;
+
+
+                CreateTable();
+
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
         }
 
         public List<Players> GetPlayers(string csvpath)
@@ -44,6 +110,15 @@ namespace beadando
                 return players;
             }
         }
+
+        
+
+
+
+
+
+
+
 
     }
 }
