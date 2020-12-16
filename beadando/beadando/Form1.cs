3cfff63292dc038c4d0b8e449dbb9b5234aee530
Norbert Bencsik <norbert.bencsik@stud.uni-corvinus.hu>
2020-12-16 20:29:16 +0100

    majdnem végleges

--- beadando/beadando/Form1.cs
+++ beadando/beadando/Form1.cs
@@ -12,6 +12,9 @@ using System.Windows.Forms;
 using System.Reflection;
 using Excel = Microsoft.Office.Interop.Excel;
 using System.Diagnostics;
+using System.Drawing.Text;
+using Spire.Xls;
+using Spire.Xls.Collections;
 
 namespace beadando
 {
@@ -72,6 +75,75 @@ namespace beadando
             Excel.Range CR = (Excel.Range)xlWorkSheet.Cells[1, 1];
             CR.Select();
             xlWorkSheet.PasteSpecial(CR, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, true);
+
+            RemoveColoumn(xlWorkSheet,"A1");
+
+            AddRow(xlWorkSheet,1);
+
+            Formazas(xlWorkSheet);
+            JatekosMinoseg(xlWorkSheet);
+
+
+        }
+        private void RemoveColoumn(Excel.Worksheet xlWorkSheet, string coloumnname) 
+        {
+            Excel.Range range = (Excel.Range)xlWorkSheet.get_Range(coloumnname, Missing.Value);
+            range.EntireColumn.Delete(Missing.Value);
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(range);            
+
+        }
+        private void AddRow(Excel.Worksheet xlWorkSheet, int rowindex)
+        {
+            Excel.Range range = (Excel.Range)xlWorkSheet.Rows[rowindex];
+            range.Insert();
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(range);
+
+            Fejlecek(xlWorkSheet);
+
+        }
+        private void Fejlecek(Excel.Worksheet xlWorkSheet) 
+        {
+            xlWorkSheet.Cells[1, 1] = "Név";
+            xlWorkSheet.Cells[1, 2] = "Pozíció";
+            xlWorkSheet.Cells[1, 3] = "Játszott perc";
+            xlWorkSheet.Cells[1, 4] = "Pontátlag";
+            xlWorkSheet.Cells[1, 5] = "Percenkénti pontok";
+
+        }
+
+        private void Formazas(Excel.Worksheet xlWorkSheet) 
+        {
+            Excel.Range headerRange = xlWorkSheet.get_Range("A1","E1");
+            headerRange.Font.Bold = true;
+            headerRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+            headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            headerRange.EntireColumn.AutoFit();
+            headerRange.RowHeight = 40;
+            headerRange.Interior.Color = Color.Fuchsia;
+            Excel.Range lastsor = xlWorkSheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell, Type.Missing);
+            Excel.Range totalRange = xlWorkSheet.get_Range("A2", "E"+lastsor.Row);
+
+            totalRange.BorderAround2(Excel.XlLineStyle.xlDash, Excel.XlBorderWeight.xlMedium);
+            totalRange.Cells.Borders.LineStyle = Excel.XlLineStyle.xlDashDot;
+            totalRange.Interior.Color = Color.Yellow;
+                
+
+            headerRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+
+
+
+
+        }
+
+        private void JatekosMinoseg(Excel.Worksheet xlWorkSheet) 
+        {
+
+            Excel.Range lastsor = xlWorkSheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell, Type.Missing);
+            Excel.Range totalRange = xlWorkSheet.get_Range("A2", "E" + lastsor.Row);
+
+            totalRange.Sort(totalRange.Columns[5], Excel.XlSortOrder.xlDescending);
+
+
         }
 
         private void Szures_Click(object sender, EventArgs e)
